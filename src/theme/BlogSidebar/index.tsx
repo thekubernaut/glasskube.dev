f8af68dc64b57f651de919d30b1005904efe31fc
Philip Miglinci <pmig@glasskube.com>
2025-03-06 10:12:02 +0100

    docs: distr vs replicated (#54)
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    Co-authored-by: jakepage91 <jake@glasskube.com>

--- src/theme/BlogSidebar/index.tsx
+++ src/theme/BlogSidebar/index.tsx
@@ -9,9 +9,14 @@ export default function BlogSidebar(): JSX.Element {
     <div className={clsx('card__header', styles.sidebar)}>
       <h3>About Glasskube</h3>
       <p>
-        We are the creators of the Glasskube Open-Source Package Manager for
-        Kubernetes and offering a comprehensive yet easy to use software
-        distribution platform.
+        Glasskube is specialized in secure software distribution. Our Open
+        Source Software Distribution Platform (
+        <a href={'https://github.com/glasskube/distr/'} target={'_blank'}>
+          Distr
+        </a>
+        ) is built for software vendors distributing their application to
+        customer controlled environments. We are also the authors of a
+        Kubernetes Package Manager.
       </p>
       <div className={styles.buttons}>
         <DemoButton additionalClassNames="" />
