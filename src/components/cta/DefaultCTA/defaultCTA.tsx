85c9f8ee837ccefe84cbbc9cfb4ec97dd31f2510
Philip Miglinci <pmig@glasskube.com>
2025-05-20 17:48:23 +0200

    chore: update Default CTA
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- src/components/cta/DefaultCTA/defaultCTA.tsx
+++ src/components/cta/DefaultCTA/defaultCTA.tsx
@@ -3,9 +3,8 @@ import CustomCTA from '../CustomCTA/customCTA';
 export default function DefaultCTA(): JSX.Element {
   return (
     <CustomCTA
-      header="Do you still think that offering self-managed installations is hard?"
-      text="Distr is a battle tested software distribution platform that helps you scale from your first
-       self-managed customers to dozens and even thousands."
+      header="Sign up and explore Distr"
+      text="Distr is a battle tested software distribution platform that helps you scale from your first self-managed customers to dozens and even thousands."
     />
   );
 }
